27a8147a3d09fa84b66aa32c6f4f1284d4b5c996
Dan Walsh <dan.a.walsh@icloud.com>
2023-06-14 01:00:48 +1000

    massive rewrite, now using lazy.vim

--- after/plugin/comment.lua
+++
@@ -1,7 +0,0 @@
--- require('Comment').setup()
-require('nvim_comment').setup({
-  -- should comment out empty or whitespace only lines
-  comment_empty = false,
-  -- Visual/Operator mapping left hand side
-  operator_mapping = "<leader>/" -- default = "gc"
-})
