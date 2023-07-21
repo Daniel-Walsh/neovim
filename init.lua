0355893bd8437d764c29dd9462afac24a1c82114
Dan Walsh <dan.a.walsh@icloud.com>
2023-07-21 19:13:07 +1000

    set <enter> for incremental selection

--- init.lua
+++ init.lua
@@ -443,8 +443,8 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
+      init_selection = '<Enter>',
+      node_incremental = '<Enter>',
       scope_incremental = '<c-s>',
       node_decremental = '<M-space>',
     },
